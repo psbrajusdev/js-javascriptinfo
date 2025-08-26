2ddde32115727d686a471009bdbc1864fc36feff
Pablo S. Brajus <psbrajusmostto@gmail.com>
2025-08-26 15:22:15 -0300

    jugando con funciones

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-alert("Hola Mundo!")
+
+
+function diHola(){
+    console.log("Hola!");
+    
+}
+
+diHola();
+
+function Edad(){
+    let edad=Number(prompt("Cual es tu edad?"));
+    if(edad <= 18){
+        alert("Eres menor de edad");
+    }else{
+        alert("Podes entrar al boliche");
+    }
+
+}
+
+Edad();
+
+function verificarEdad(edad){
+    if(edad <= 18){
+        alert("Eres menor de edad, segunda funcion");
+    }else{
+        alert("Podes entrar al boliche, segun funcion");
+    }
+}
+
+edad=prompt("Cual es tu edad?");
+verificarEdad(edad);    
